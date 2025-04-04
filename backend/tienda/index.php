fc0dea2ed64b37b54f1b8a1f2dda5e535cb71834
SAMKING25 <samutrujillogomez@gmail.com>
2025-04-04 23:21:55 +0200

    iniciando el navbar y sidebar

--- backend/tienda/index.php
+++ backend/tienda/index.php
@@ -1,83 +1,189 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="estilos/sidebar.css">
     <?php
-        error_reporting( E_ALL );
-        ini_set("display_errors", 1 );  
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
 
-        require('util/conexion.php');
+    require('util/conexion.php');
 
-        // session_start();
-        // if (isset($_SESSION["usuario"])) {
-        //     echo "<h2>Bienvenid@ " . $_SESSION["usuario"] .  "</h2>"; ?>
-            <!-- <a class="btn btn-warning" href="usuario/cerrar_sesion.php">Cerrar sesion</a> 
-            <a class="btn btn-primary" href="usuario/cambiar_credenciales.php?usuario=<?php // echo $_SESSION["usuario"] ?>">Cambiar credenciales</a> -->
-        <?php  // } else { ?>
-            <!-- <a class="btn btn-warning" href="usuario/iniciar_sesion.php">Iniciar sesion</a> -->
-        <?php // } ?>
+    // session_start();
+    // if (isset($_SESSION["usuario"])) {
+    //     echo "<h2>Bienvenid@ " . $_SESSION["usuario"] .  "</h2>"; 
+    ?>
+    <!-- <a class="btn btn-warning" href="usuario/cerrar_sesion.php">Cerrar sesion</a> 
+            <a class="btn btn-primary" href="usuario/cambiar_credenciales.php?usuario=<?php // echo $_SESSION["usuario"] 
+                                                                                        ?>">Cambiar credenciales</a> -->
+    <?php  // } else { 
+    ?>
+    <!-- <a class="btn btn-warning" href="usuario/iniciar_sesion.php">Iniciar sesion</a> -->
+    <?php // } 
+    ?>
 </head>
+
 <body>
-    <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST") {
+    <header class="p-3 bg-dark text-white">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                        <use xlink:href="#bootstrap" />
+                    </svg>
+                </a>
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 text-secondary">Inicio</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Productos</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+                </ul>
+
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                    <input type="search" class="form-control form-control-dark" placeholder="Buscar..." aria-label="Search">
+                </form>
+
+                <div class="text-end">
+                    <button type="button" class="btn btn-outline-light me-2">Login</button>
+                    <button type="button" class="btn btn-inf">Sign-up</button>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="d-flex">
+        <div class="d-flex flex-column flex-shrink-0 p-3 bg-light sidebar" style="width: 280px;">
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                <svg class="bi me-2" width="40" height="32">
+                    <use xlink:href="#bootstrap" />
+                </svg>
+                <span class="fs-4">SAMAS home</span>
+            </a>
+            <hr>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li class="nav-item">
+                    <a href="#" class="nav-link active" aria-current="page">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#home" />
+                        </svg>
+                        Inicio
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#speedometer2" />
+                        </svg>
+                        Productos
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#table" />
+                        </svg>
+                        Suscripción
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#grid" />
+                        </svg>
+                        Products
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link link-dark">
+                        <svg class="bi me-2" width="16" height="16">
+                            <use xlink:href="#people-circle" />
+                        </svg>
+                        Ajustes
+                    </a>
+                </li>
+            </ul>
+            <hr>
+            <div class="dropdown">
+                <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/justmanuva.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                    <strong>justmanuva</strong>
+                </a>
+                <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                    <li><a class="dropdown-item" href="#">New project...</a></li>
+                    <li><a class="dropdown-item" href="#">Settings</a></li>
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Sign out</a></li>
+                </ul>
+            </div>
+        </div>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $id_producto = $_POST["id_producto"];
-            //  borrar el producto
+
             $sql = "DELETE FROM productos WHERE id_producto = '$id_producto'";
-            $_conexion -> query($sql);
+            $_conexion->query($sql);
         }
 
         $sql = "SELECT * FROM productos";
-        $resultado = $_conexion -> query($sql);
-    ?>
+        $resultado = $_conexion->query($sql);
+        ?>
 
-    <div class="container">
-        <h1>Tabla Inicio</h1>
-        <div class="mb-3">
-            <?php if (isset($_SESSION["usuario"])) { ?>
-                <a href="productos/index.php" class="btn btn-success">Ir a tabla de productos</a>
-            <?php } ?>
-            
-        </div>
-        <table class="table table-info table-striped table-hover">
-            <thead class="table-dark">
-                <tr>
-                    <th>Nombre</th>
-                    <th>Precio</th>
-                    <th>Categoria</th>
-                    <th>Stock</th>
-                    <th>Descripcion</th>
-                    <th>Medidas</th>
-                    <th>Imagen</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    while($fila = $resultado -> fetch_assoc()){
+        <div class="container">
+            <h1>Tabla Inicio</h1>
+            <div class="mb-3">
+                <?php if (isset($_SESSION["usuario"])) { ?>
+                    <a href="productos/index.php" class="btn btn-success">Ir a tabla de productos</a>
+                <?php } ?>
+
+            </div>
+            <table class="table table-striped table-hover">
+                <thead class="table-dark">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Categoria</th>
+                        <th>Stock</th>
+                        <th>Descripcion</th>
+                        <th>Medidas</th>
+                        <th>Imagen</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    while ($fila = $resultado->fetch_assoc()) {
                         echo "<tr>";
-                        echo "<td>" . $fila["nombre"] ."</td>";
-                        echo "<td>" . $fila["precio"] ."</td>";
-                        echo "<td>" . $fila["categoria"] ."</td>";
-                        echo "<td>" . $fila["stock"] ."</td>";
-                        ?>
+                        echo "<td>" . $fila["nombre"] . "</td>";
+                        echo "<td>" . $fila["precio"] . "</td>";
+                        echo "<td>" . $fila["categoria"] . "</td>";
+                        echo "<td>" . $fila["stock"] . "</td>";
+                    ?>
                         <td><?php echo $fila["descripcion"] ?></td>
                         <td>
                             <?php
-                                echo $fila["largo"]."cm x".$fila["ancho"]."cm x".$fila["alto"]."cm";
+                            echo $fila["largo"] . "cm x" . $fila["ancho"] . "cm x" . $fila["alto"] . "cm";
                             ?>
-                         </td>
+                        </td>
                         <td>
                             <img width="160" height="200" src="imagenes/<?php echo $fila["imagen"] ?>">
                         </td>
                         </tr>
                     <?php } ?>
-            </tbody>
-        </table>
-        
+                </tbody>
+            </table>
+
+        </div>
     </div>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
