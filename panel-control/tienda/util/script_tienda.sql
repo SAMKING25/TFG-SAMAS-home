902a424b60376e764f9265e24130b5bb9126c122
justmanuva <manuva2662004@gmail.com>
2025-04-06 22:49:45 +0200

    Script tienda actualizado

--- panel-control/tienda/util/script_tienda.sql
+++ panel-control/tienda/util/script_tienda.sql
@@ -1,23 +1,54 @@
-CREATE SCHEMA prueba_samas_db;
-USE prueba_samas_db;
+CREATE SCHEMA prueba_samas;
+
+USE prueba_samas;
+
+DROP TABLE categorias;
+DROP TABLE usuarios;
+DROP TABLE proveedores;
+DROP TABLE productos;
+
+INSERT INTO categorias (categoria) VALUES ('Electrónica');
+
+INSERT INTO usuarios (email, nombre, contrasena, suscripcion, proveedor) VALUES
+    ('mva0025', 'Manuva', 'SAMAShome1234', 'Premium', TRUE);
+
+INSERT INTO proveedores (email_proveedor, nombre_proveedor, contrasena, foto_proveedor) VALUES
+	('mueblesbuffalo@gmail.com', 'Muebles Buffalo', 'SAMAShome1234', 'usuario-estandar.png');
+    
+INSERT INTO productos (nombre, precio, categoria, stock, descripcion, largo, ancho, alto, id_proveedor, imagen) VALUES
+('Smartphone', 499.99, 'Electrónica', 10, 'Teléfono inteligente de última generación', 15, 7, 1, 1, 'sillon.jpg');
+
+INSERT INTO categorias (categoria) VALUES ('Sofás');
+INSERT INTO categorias (categoria) VALUES ('Sillas');
+INSERT INTO categorias (categoria) VALUES ('Mesas');
+INSERT INTO categorias (categoria) VALUES ('Colchones');
+INSERT INTO categorias (categoria) VALUES ('Armarios');
 
 CREATE TABLE categorias (
-	categoria VARCHAR(30) PRIMARY KEY
+	categoria VARCHAR(50) PRIMARY KEY
 );
 
 CREATE TABLE usuarios (
-    id_usuario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	id_usuario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     email VARCHAR(50),
     nombre VARCHAR(50),
     contrasena VARCHAR(120),
     suscripcion VARCHAR(20),
-    proveedor BOOLEAN
+    foto_usuario VARCHAR(60)
+);
+
+CREATE TABLE proveedores (
+	id_proveedor INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email_proveedor VARCHAR(50),
+    nombre_proveedor VARCHAR(50),
+    contrasena VARCHAR(120),
+    foto_proveedor VARCHAR(60)
 );
 
 CREATE TABLE productos (
-	id_producto INT PRIMARY KEY AUTO_INCREMENT,
+	id_producto INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(50),
-    precio NUMERIC(6,2),
+    precio NUMERIC(4,2),
     categoria VARCHAR(30),
     stock INT DEFAULT 0,
     descripcion VARCHAR(255),
@@ -27,5 +58,5 @@ CREATE TABLE productos (
     id_proveedor INT,
     imagen VARCHAR(60),
     FOREIGN KEY (categoria) REFERENCES categorias(categoria),
-    FOREIGN KEY (id_proveedor) REFERENCES usuarios(id_usuario)
+    FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor)
 );
