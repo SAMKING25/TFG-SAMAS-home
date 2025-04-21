1f55e7cb385c772ad07e13cc7717dd4ecd17e37c
alvarooo32 <alvaro23.ossa@gmail.com>
2025-04-21 14:43:39 +0200

    Pasarela de pago Paypal: Solo para pruebas sandbox

---
+++ pasarela-pago/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>paypal</title>
+
+    <script src="https://www.paypal.com/sdk/js?client-id=AZiNIbkuxCM_s2y_iYwPg7V4zhQKzZbSJhN_y0P7_Pl5hDT7l3bAdsy8VoRzicjIA7r3JnzwT8e_TTJK&currency=EUR"></script>
+</head>
+<body>
+    <div id="paypal-buttom-conteiner"></div>
+    /** Esta creado para hacer prueba con sandbox el cual paypal te da para poder hacer pruebas de pagos para los 
+    desarrolladores, te dan un usuario y contraseña fake para hacer el pago */
+    <script>
+        paypal.Buttons({
+            createOrder: function(data, actions){
+                return actions.order.create({
+                    purchase_units:[{
+                        amount:{
+                            value:100 //precio
+                        }
+                    }]
+                });
+            },
+            onApprove: function(data, actions){
+                actons.order.capture().then(function(detalles){
+                   windows.location.href="completado.html";
+                });
+            },
+
+            onCancel: function(data){
+                alert("Pago cancelado");
+                console.log(data);
+            }
+        }).render("#paypal-buttom-conteiner");
+    </script>
+</body>
+</html>
