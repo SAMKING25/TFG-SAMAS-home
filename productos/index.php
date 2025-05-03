c60bd8d8f410303739b884f2684902f18b66e023
sergioam3005@gmail.com <sergioam3005@gmail.com>
2025-05-03 13:06:00 +0200

    creo index de productos

---
+++ productos/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SAMAS HOME</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <link rel="shortcut icon" href="./img/logos/logo-marron-nobg.ico" />
+    <!-- Archivo CSS personalizado -->
+    <link rel="stylesheet" href="/css/landing.css" />
+    <!--search-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
+    <!--conexion con BD-->
+    <?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+    require('../util/conexion.php');
+
+    session_start();
+    ?>
+</head>
+
+<body>
+    <?php include('../navbar.php'); ?>
+    <div class="container">
+    </div>
+</body>
+
+</html>
