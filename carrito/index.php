82626099004a92de4b73e9dac506e4d23023096e
justmanuva <manuva2662004@gmail.com>
2025-05-05 15:33:33 +0200

    Plantilla carrito lista

--- carrito/index.php
+++ carrito/index.php
@@ -5,7 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carrito</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+	<link rel="shortcut icon" href="./img/logos/logo-marron-nobg.ico" />
+	<!-- Archivo CSS personalizado -->
+	<link rel="stylesheet" href="/css/landing.css" />
+	<!--search-->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
     <style>
         @media (min-width: 1025px) {
             .h-custom {
@@ -27,7 +34,8 @@
 </head>
 
 <body>
-    <section class="h-100 h-custom" style="background-color: #d2c9ff;">
+    <?php include("../navbar.php"); ?>
+    <section class="h-100 h-custom" style="background-color: #F7E5CB">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12">
@@ -38,7 +46,7 @@
                                     <div class="p-5">
                                         <div class="d-flex justify-content-between align-items-center mb-5">
                                             <h1 class="fw-bold mb-0">Carrito</h1>
-                                            <h6 class="mb-0 text-muted">3 items</h6>
+                                            <h6 class="mb-0 text-muted">X artículos</h6>
                                         </div>
                                         <hr class="my-4">
 
@@ -48,8 +56,8 @@
                                                     class="img-fluid rounded-3" alt="Cotton T-shirt">
                                             </div>
                                             <div class="col-md-3 col-lg-3 col-xl-3">
-                                                <h6 class="text-muted">Shirt</h6>
-                                                <h6 class="mb-0">Cotton T-shirt</h6>
+                                                <h6 class="text-muted">Categoría</h6>
+                                                <h6 class="mb-0">Nombre</h6>
                                             </div>
                                             <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
                                                 <button data-mdb-button-init data-mdb-ripple-init
@@ -68,7 +76,7 @@
                                                 </button>
                                             </div>
                                             <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                                <h6 class="mb-0">€ 44.00</h6>
+                                                <h6 class="mb-0">44.00€</h6>
                                             </div>
                                             <div class="col-md-1 col-lg-1 col-xl-1 text-end">
                                                 <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
@@ -83,8 +91,8 @@
                                                     class="img-fluid rounded-3" alt="Cotton T-shirt">
                                             </div>
                                             <div class="col-md-3 col-lg-3 col-xl-3">
-                                                <h6 class="text-muted">Shirt</h6>
-                                                <h6 class="mb-0">Cotton T-shirt</h6>
+                                                <h6 class="text-muted">Categoría</h6>
+                                                <h6 class="mb-0">Nombre</h6>
                                             </div>
                                             <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
                                                 <button data-mdb-button-init data-mdb-ripple-init
@@ -103,7 +111,7 @@
                                                 </button>
                                             </div>
                                             <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                                <h6 class="mb-0">€ 44.00</h6>
+                                                <h6 class="mb-0">44.00€</h6>
                                             </div>
                                             <div class="col-md-1 col-lg-1 col-xl-1 text-end">
                                                 <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
@@ -118,8 +126,8 @@
                                                     class="img-fluid rounded-3" alt="Cotton T-shirt">
                                             </div>
                                             <div class="col-md-3 col-lg-3 col-xl-3">
-                                                <h6 class="text-muted">Shirt</h6>
-                                                <h6 class="mb-0">Cotton T-shirt</h6>
+                                                <h6 class="text-muted">Categoría</h6>
+                                                <h6 class="mb-0">Nombre</h6>
                                             </div>
                                             <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
                                                 <button data-mdb-button-init data-mdb-ripple-init
@@ -138,7 +146,7 @@
                                                 </button>
                                             </div>
                                             <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                                <h6 class="mb-0">€ 44.00</h6>
+                                                <h6 class="mb-0">44.00€</h6>
                                             </div>
                                             <div class="col-md-1 col-lg-1 col-xl-1 text-end">
                                                 <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
@@ -146,55 +154,38 @@
                                         </div>
 
                                         <hr class="my-4">
-
-                                        <div class="pt-5">
-                                            <h6 class="mb-0"><a href="/index.php" class="text-body"><i
-                                                        class="fas fa-long-arrow-alt-left me-2"></i>Back to shop</a>
-                                            </h6>
-                                        </div>
                                     </div>
                                 </div>
                                 <div class="col-lg-4 bg-body-tertiary">
                                     <div class="p-5">
-                                        <h3 class="fw-bold mb-5 mt-2 pt-1">Summary</h3>
+                                        <h3 class="fw-bold mb-5 mt-2 pt-1">Resumén del pedido</h3>
                                         <hr class="my-4">
 
                                         <div class="d-flex justify-content-between mb-4">
-                                            <h5 class="text-uppercase">items 3</h5>
-                                            <h5>€ 132.00</h5>
+                                            <h5 class="text-uppercase">Artículos X</h5>
+                                            <h5>132.00€</h5>
                                         </div>
 
-                                        <h5 class="text-uppercase mb-3">Shipping</h5>
-
-                                        <div class="mb-4 pb-2">
-                                            <select data-mdb-select-init>
-                                                <option value="1">Standard-Delivery- €5.00</option>
-                                                <option value="2">Two</option>
-                                                <option value="3">Three</option>
-                                                <option value="4">Four</option>
-                                            </select>
-                                        </div>
-
-                                        <h5 class="text-uppercase mb-3">Give code</h5>
+                                        <h5 class="text-uppercase mb-3">Código de descuento</h5>
 
                                         <div class="mb-5">
                                             <div data-mdb-input-init class="form-outline">
                                                 <input type="text" id="form3Examplea2"
                                                     class="form-control form-control-lg" />
-                                                <label class="form-label" for="form3Examplea2">Enter your code</label>
+                                                <label class="form-label" for="form3Examplea2">Introduce tu código</label>
                                             </div>
                                         </div>
 
                                         <hr class="my-4">
 
                                         <div class="d-flex justify-content-between mb-5">
-                                            <h5 class="text-uppercase">Total price</h5>
-                                            <h5>€ 137.00</h5>
+                                            <h5 class="text-uppercase">Total</h5>
+                                            <h5>137.00€</h5>
                                         </div>
 
-                                        <button type="button" data-mdb-button-init data-mdb-ripple-init
+                                        <a href="../pasarela-pago" data-mdb-button-init data-mdb-ripple-init
                                             class="btn btn-dark btn-block btn-lg"
-                                            data-mdb-ripple-color="dark">Register</button>
+                                            data-mdb-ripple-color="dark">Ir a pago</a>
 
                                     </div>
                                 </div>
@@ -205,6 +196,7 @@
             </div>
         </div>
     </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
