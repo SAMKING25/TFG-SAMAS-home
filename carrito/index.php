6d634d309cec5075ce8c539fc2392f289bb41d28
justmanuva <manuva2662004@gmail.com>
2025-05-26 20:21:10 +0200

    Carrito: Script bootstrap corregido

--- carrito/index.php
+++ carrito/index.php
@@ -134,5 +134,6 @@ while ($fila = $resultado->fetch_assoc()) {
     </div>
 
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>
