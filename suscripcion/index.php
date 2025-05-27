2a16c97e962a86e9ad3f0c16bd24d0144e493f7d
alsowski <alevilca05@gmail.com>
2025-05-27 11:53:36 +0200

    icono del navbar y suscripciones actualizados

--- suscripcion/index.php
+++ suscripcion/index.php
@@ -171,12 +171,12 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x text-danger feature-icon" viewBox="0 0 16 16">
                                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
                                         </svg>
-                                        Descuentos Emergentes
+                                        Descuentos especiales
                                     <?php else: ?>
                                         <svg class="feature-icon text-success" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
                                         </svg>
-                                        Descuentos Emergentes
+                                        Descuentos especiales
                                     <?php endif; ?>
                                 </li>
                                 <li class="mb-3">
