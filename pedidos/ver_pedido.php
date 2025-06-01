bb97f4ec86cb5be2cdf71c3820c44e2ae5592688
SAMKING25 <samutrujillogomez@gmail.com>
2025-06-01 16:13:37 +0200

    Añado los viewports

--- pedidos/ver_pedido.php
+++ pedidos/ver_pedido.php
@@ -41,6 +41,7 @@ $productos = $stmt->get_result();
     <title>Detalle del pedido</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Archivo CSS personalizado -->
     <link rel="stylesheet" href="/css/landing.css" />
     <style>
